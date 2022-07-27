b93037fa9d9de016073fd9497e9d44f20694f3ca
raman186 <goyalraman755@gmail.com>
2022-07-27 19:45:27 +0530

    portfolio ready

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -39,6 +39,15 @@ const Portfolio = () => {
           <br />
           <br />
           <div className="features">
+            <span style={{color: darkMode?'white': ''}}> Summary-</span><br />
+            <span style={{color:"gray", fontSize:".8rem"}}>  
+              A young company of music lovers, for music lovers. Let’s help you find your dream gear. 
+              Curated products. 250,000+ happy customers. 70+ brands. Express shipping. Easy returns. 
+              We're Customer Obsessed. We've Completed 10 Years. Begin Audiophile Journey.
+            </span>
+            <br />
+          </div>
+          <div className="features">
             <span style={{color: darkMode?'white': ''}}> Features -</span><br />
             <span style={{color:"gray", fontSize:".8rem"}}> ●	Social Sign-in/Sign-up</span>
             <span style={{color:"gray", fontSize:".8rem"}}> ●	Add to Cart Functionality</span>
@@ -51,8 +60,11 @@ const Portfolio = () => {
             <span style={{color:"gray", fontSize:".8rem"}}> 	JavaScript</span><br /><br />
           </div>
 
-          <a href='https://unique-sprite-5a80fe.netlify.app/' target="blank"><button className="c-button" >Check it</button></a>
-
+          <div className="buttons">
+            <a href='https://unique-sprite-5a80fe.netlify.app/' target="blank"><button className="c-button" >Check Website</button></a>
+            <a href='https://github.com/rgoyal0001/Headphone-Zone-Clone' target="blank"><button className="c-button" >Check Code</button></a>
+          </div>
+          
         </div>
         <br />
         <div className="project-items">
@@ -63,6 +75,15 @@ const Portfolio = () => {
           <span style={{color:"orange"}}>Spotify</span>
           <br />
           <br />
+          <div className="features">
+            <span style={{color: darkMode?'white': ''}}> Summary-</span><br />
+            <span style={{color:"gray", fontSize:".8rem"}}>  
+              Spotify is a proprietary Swedish audio streaming and media services provider . 
+              It is one of the largest music streaming service providers, with over 422 million monthly active users. 
+              This is team project with 5 member team. Here we have added home page with web player and searching page with all functionalities.
+            </span>
+            <br />
+          </div>
           <div className="features">
             <span style={{color: darkMode?'white': ''}}> Features -</span><br />
             <span style={{color:"gray", fontSize:".8rem"}}> ●	Social Sign-in/Sign-up</span>
@@ -77,9 +98,11 @@ const Portfolio = () => {
             <span style={{color:"gray", fontSize:".8rem"}}> 	JavaScript</span> |
             <span style={{color:"gray", fontSize:".8rem"}}> 	JSON</span><br /><br />
           </div>
-
-          <a href='https://rgoyal0001.github.io/Spotify_Clone/' target="blank"><button className="c-button" >Check it</button></a>
-
+          <div className="buttons">
+            <a href='https://rgoyal0001.github.io/Spotify_Clone/' target="blank"><button className="c-button" >Check Website</button></a>
+            <a href='https://github.com/rgoyal0001/Spotify_Clone' target="blank"><button className="c-button" >Check Code</button></a>
+          </div>
+          
         </div>
       </div>
     </div>
